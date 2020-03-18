a96eb65b98c53f50309d96eb3bd1cc530cfdb45a
Jaety Edwards <jaetye@gmail.com>
2020-03-18 21:52:16 +0000

    adding more entities

--- extractor.py
+++ extractor.py
@@ -26,8 +26,16 @@ if __name__=="__main__":
                 qid = pydash.get(record, 'id')
                 label = pydash.get(record, 'labels.en.value')
                 desc = pydash.get(record, 'descriptions.en.value')
+                birth = pydash.get(record, 'claims.P569[0].mainsnak.datavalue.value')
+                death = pydash.get(record, 'claims.P570[0].mainsnak.datavalue.value')
+                birth_place = pydash.get(record, 'claims.P19[0].mainsnak.datavalue.value.id')
+                death_place = pydash.get(record, 'claims.P19[0].mainsnak.datavalue.value.id')
                 print(qid, label)
                 print("\t", desc)
-        if count > 100:
+                print("\tbirth:", birth)
+                print("\tbirth place:", birth_place)
+                print("\tdeath:", death)
+                print("\tdeath place:", death_place)
+        if count > 10:
             break            
 
